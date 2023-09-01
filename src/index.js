ceef1380e0e830d6587e4feef89df6f62f881b3a
MorskoySom <Somovvitvit@gmail.com>
2023-09-01 16:32:32 +0300

    5656

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ const elem = {
     form: document.querySelector(`#search-form`),
     gallery: document.querySelector(`.gallery`)
 }
-console.log(elem.form);
 
 elem.form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
     evt.preventDefault();
-    console.log(elem.form[0].value.trim());
     let query = elem.form[0].value.trim();
 
     async function fetchQuerry(query) {
@@ -24,21 +22,25 @@ function handlerSubmit(evt) {
             per_page: 40
         })
         return await axios.get(`https://pixabay.com/api/?${params}`)
+
     }
     fetchQuerry(query)
-        .then(resp => elem.gallery.innerHTML = createMarkup(resp.data.hits))
-        .catch(err => console.log(err))
+        .then((resp) => {
+            elem.gallery.innerHTML = createMarkup(resp.data.hits)
+            Notiflix.Notify.success(`Hooray! We found ${resp.data.totalHits} images`)
+        })
+        .catch(err => Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'))
 
     function createMarkup(arr) {
         return arr.map(({ likes, views, comments, downloads, tags, webformatURL, largeImageURL }) => `<div class="photo-card">
-            <div class="img-size">
+            
                 <img class="img-size" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </div>
+            
             <div class="info">
                 <p class="info-item"><b>Likes</b><br /> ${likes}</p>                
                 <p class="info-item"><b>Views</b><br /> ${views}</p>
-                <p class="info-item"><b>Comments:</b><br /> ${comments}</p>
-                <p class="info-item"><b>Downloads:</b><br /> ${downloads}</p>
+                <p class="info-item"><b>Comments</b><br /> ${comments}</p>
+                <p class="info-item"><b>Downloads</b><br /> ${downloads}</p>
             </div>
             </div>`).join('');
     }
