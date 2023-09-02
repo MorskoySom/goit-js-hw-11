d1ba28adea634902bd2762bd79846b004b049e4f
MorskoySom <Somovvitvit@gmail.com>
2023-09-02 14:54:41 +0300

    77

--- src/index.js
+++ src/index.js
@@ -3,16 +3,30 @@ import Notiflix from 'notiflix';
 
 const elem = {
     form: document.querySelector(`#search-form`),
-    gallery: document.querySelector(`.gallery`)
+    gallery: document.querySelector(`.gallery`),
+    guard: document.querySelector(`.js-guard`)
 }
 
+let currentPage = 1;
+
+const options = {
+    rootMargin: "300px",
+}
+const observer = new IntersectionObserver(handlerLoadMore, options);
+
 elem.form.addEventListener('submit', handlerSubmit);
+
+function handlerLoadMore(entries) {
+    entries.forEach((entry) => {
+    });
+}
 
 function handlerSubmit(evt) {
     evt.preventDefault();
     let query = elem.form[0].value.trim();
+    // let currentPage = 1;
 
-    async function fetchQuerry(query) {
+    async function fetchQuerry(query, currentPage = `1`) {
         elem.gallery.innerHTML = ``
         const params = new URLSearchParams({
             key: "39170790-720d13338eae2dc65ab148b0f",
@@ -20,19 +34,22 @@ function handlerSubmit(evt) {
             image_type: "photo",
             orientation: "horizontal",
             safesearch: true,
-            per_page: 40
+            per_page: 40,
+            page: currentPage
         })
         return await axios.get(`https://pixabay.com/api/?${params}`)
     }
     fetchQuerry(query)
         .then((resp) => {
-            console.log(resp.data);
+            // console.log(resp.data);
             if (resp.data.totalHits === 0) {
                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
             } else {
-                elem.gallery.innerHTML = createMarkup(resp.data.hits)
+                // elem.gallery.innerHTML = createMarkup(resp.data.hits)
+                elem.gallery.insertAdjacentHTML(createMarkup(resp.data.hits))
                 Notiflix.Notify.success(`Hooray! We found ${resp.data.totalHits} images`)
             }
+            observer.observe(elem.guard);
         })
         .catch(err => Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'))
 
